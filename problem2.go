5cc17be1cd98ce2815f6880ebbe5c55b41df61c4
Harrishaqfer <154215094+Harrishaqfer@users.noreply.github.com>
2024-01-15 17:14:40 +0530

    Add files via upload

---
+++ problem2.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	Go "godaaay2/gofunctions"
+)
+
+func main() {
+
+	studentF := Go.Createf("student.csv")
+	marksF := Go.Createf("marks.csv")
+	s, m := Go.Initialize(studentF, marksF)
+	avg, total := Go.Average(m, s)
+	fmt.Println(avg)
+	sm := Go.Student_vise_marks(m, s)
+	fmt.Println(sm, "\n\n")
+	//Go.Order_vise(sm, s)
+	//student_id := "S1"
+	//Go.Info(student_id, s, avg, sm, total)
+	fmt.Println(avg, total)
+
+}
